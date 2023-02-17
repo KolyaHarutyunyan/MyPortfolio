0716c1337891f5c064a872804862c80c37ac2e22
KolyaHarutyunyan <kolyah2809@gmail.com>
2023-02-17 16:52:22 +0400

    add professional skills

--- src/components/qualifications/Qualifications.tsx
+++ src/components/qualifications/Qualifications.tsx
@@ -1,42 +1,60 @@
 const Qualifications = () => {
     return (
-        <div className="qualifications">
-            <span className="quali-text">Professional Skills</span>
-            <div className="qualifications-container">
-                <div className="qualification">
-                    <div className="content html5" />
-                    <h1>html5</h1>
-                </div>
-                <div className="qualification">
-                    <div className="content css3" />
-                    <h1>css3</h1>
-                </div>
-                <div className="qualification">
-                    <div className="content sass" />
-                    <h1>sass</h1>
-                </div>
-                <div className="qualification">
-                    <div className="content js" />
-                    <h1>javascript</h1>
-                </div>
-                <div className="qualification">
-                    <div className="content react" />
-                    <h1>react</h1>
-                </div>
-                <div className="qualification">
-                    <div className="content ts" />
-                    <h1>typescript</h1>
-                </div>
-                <div className="qualification">
-                    <div className="content npm" />
-                    <h1>npm</h1>
-                </div>
-                <div className="qualification">
-                    <div className="content git" />
-                    <h1>git</h1>
-                </div>
+      <div className="qualifications">
+        <span className="quali-text">Professional Skills</span>
+        <div className="qualifications-container">
+          <div className="qualification pro-skill">
+            <div className="pro-skill-block">
+              <div className="content html5" />
+              <div className="content css3" />
             </div>
+            <div className="pro-skill-block">
+              <div className="content sass" />
+              <div className="content js" />
+            </div>
+          </div>
+          <div className="qualification pro-skill">
+            <div className="pro-skill-block">
+              <div className="content react" />
+              <div className="content remix" />
+            </div>
+            <div className="pro-skill-block">
+              <div className="content node" />
+              <div className="content express" />
+            </div>
+          </div>
+          <div className="qualification pro-skill">
+            <div className="pro-skill-block">
+              <div className="content tailwind" />
+              <div className="content material" />
+            </div>
+            <div className="pro-skill-block">
+              <div className="content bootstrap" />
+              <div className="content styled-components" />
+            </div>
+          </div>
+          <div className="qualification pro-skill">
+            <div className="pro-skill-block">
+              <div className="content jira" />
+              <div className="content trello" />
+            </div>
+            <div className="pro-skill-block">
+              <div className="content slack" />
+              <div className="content space" />
+            </div>
+          </div>
+          <div className="qualification pro-skill">
+            <div className="pro-skill-block">
+              <div className="content ts" />
+              <div className="content swagger" />
+            </div>
+            <div className="pro-skill-block">
+              <div className="content npm" />
+              <div className="content git" />
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
