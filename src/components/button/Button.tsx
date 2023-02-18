f3406ff9515abd2030911b77aaf94b9c7bd3db0a
KolyaHarutyunyan <kolyah2809@gmail.com>
2023-02-18 21:15:15 +0400

    add cv to download

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,13 +1,13 @@
 const Buttons = () => {
     return (
-        <div className="container buttons-container">
-            <a href="#about" className="btn pri">
-                Learn More
-            </a>
-            <a href="#contact" className="btn sec">
-                Get in Touch
-            </a>
-        </div>
+      <div className="container buttons-container">
+        <a href="../../../public/cv.pdf" download="cv.pdf" className="btn pri">
+          Get my CV
+        </a>
+        <a href="#contact" className="btn sec">
+          Get in Touch
+        </a>
+      </div>
     );
 };
 
